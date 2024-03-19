f5b865c966f7d2a2d1495f05f61d6740b2970e44
Psyon <sayan.h2020@gmail.com>
2024-03-19 13:38:55 +0330

    create rune

---
+++ DT/rune/rune.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main () {
+	char1:= 128512
+
+	char2:= 128516
+
+	char3:= 128525
+
+	fmt.Printf("char1: %d %T", char1, char1)
+	fmt.Printf("char1: %d %T", char2, char2)
+	fmt.Printf("char1: %d %T", char3, char3)
+
+}
