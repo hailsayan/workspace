f85c428b2c333009cbd409103dba9e000dea5525
Psyon <sayan.h2020@gmail.com>
2024-03-20 23:23:44 +0330

    declaration Vars

--- DT/rune/rune.go
+++ DT/rune/rune.go
@@ -1,15 +1,50 @@
 package main
-import "fmt"
 
-func main () {
-	char1:= 128512
+import (
+	"fmt"
+	"unsafe"
+)
 
-	char2:= 128516
+func main() {
+	char1 := '😀'
 
-	char3:= 128525
+	char2 := '😄'
+	char3 := 128525
 
-	fmt.Printf("char1: %d %T", char1, char1)
-	fmt.Printf("char1: %d %T", char2, char2)
-	fmt.Printf("char1: %d %T", char3, char3)
+	fmt.Printf("char1: %d %T\n", char1, char1)
+
+	fmt.Printf("char1: %d %T\n", char2, char2)
+	fmt.Printf("char1: %c %T\n", char3, char3)
+
+	myStr := "Hello World 😀 😄"
+
+	fmt.Printf("myStr: %s %T, len: %d, size: %d \n ", myStr, myStr, len(myStr), unsafe.Sizeof(myStr))
+
+	for i := 0; i < len(myStr); i++ {
+		fmt.Printf("myStr[%d]: %c %T\n", i, myStr[i], myStr[i])
+	}
+
+	myRune := []rune("Hello World 😀 😄")
+
+	fmt.Printf("myRune: %v %T, len: %d, size: %d \n ", myRune, myRune, len(myRune), unsafe.Sizeof(myRune))
+
+	for i := 0; i < len(myRune); i++ {
+		fmt.Printf("myStr[%d]: %c %T\n", i, myRune[i], myRune[i])
+	}
+
+	myRune = []rune("سلام دنیا")
+	myStr = "سلام دنیا"
+
+	fmt.Printf("myStr: %s %T, len: %d, size: %d \n ", myStr, myStr, len(myStr), unsafe.Sizeof(myStr))
+
+	for i := 0; i < len(myStr); i++ {
+		fmt.Printf("myStr[%d]: %c %T\n", i, myStr[i], myStr[i])
+	}
+
+	fmt.Printf("myRune: %v %T, len: %d, size: %d \n ", myRune, myRune, len(myRune), unsafe.Sizeof(myRune))
+
+	for i := 0; i < len(myRune); i++ {
+		fmt.Printf("myStr[%d]: %c %T\n", i, myRune[i], myRune[i])
+	}
 
 }
